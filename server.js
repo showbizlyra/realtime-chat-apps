814b3654034f6a21bf01f1fafff2218a124ad8bc
showbizlyra <just.muhammadridwan@gmail.com>
2019-04-03 22:12:49 +0700

    update procfile

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("Hello World");
 });
 
